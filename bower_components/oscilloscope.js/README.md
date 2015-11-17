83b675558470178e0bfe91e5926467e344575bd2
Sam Bellen <sambellen@mbp-van-sam.home>
2015-11-17 20:16:47 +0100

    Add oscilloscope example

---
+++ bower_components/oscilloscope.js/README.md
@@ -0,0 +1,76 @@
+# oscilloscope.js
+A small javascript plugin to create an oscilloscope of an audio-context.
+
+## Install
+```
+bower install oscilloscope.js
+```
+
+## Basic setup
+```javascript
+// Create an audio-context
+var audioContext = new window.AudioContext(),
+    oscillator = audioContext.createOscillator();
+    oscillator.type = 'sine';
+    oscillator.frequency.value = 400;
+
+// Create an Oscilloscope instance
+//   Parameters:
+//     - The container in which the oschilloscope gets created
+//     - an optional audio-context on which the oscilloscope creates an analyser-node,
+//          and can connect to the destination.
+//          If no audio-context is specified, a new one will be created created.
+var oscilloscope = new Oscilloscope('.js-oscilloscope', audioContext);
+
+// Connect the oscillator-node to the oscilloscope
+oscillator.connect(oscilloscope.analyserNode);
+
+// Start the oscilloscope
+oscilloscope.start();
+```
+## API
+### Properties
+```javascript
+Oscilloscope.target // Container
+
+Oscilloscope.width // The oscilloscope' width
+
+Oscilloscope.height // The oscilloscope' height
+
+Oscilloscope.svg // The svg-element in which we draw the oscilloscope
+
+Oscilloscope.wave // The path svg-element which represents the audio wave
+
+Oscilloscope.audioContext // The oscilloscope' audio-context
+
+Oscilloscope.running // Indicates if the oscilloscope is running
+
+Oscilloscope.hasAudio // Indicates if the oscilloscope is connected to the audio-context' destination
+```
+
+### Methods
+```javascript
+/**
+ * Start the oscilloscope
+ */
+Oscilloscope.start();
+
+/**
+ * Stop the oscilloscope
+ */
+Oscilloscope.stop();
+
+/**
+ * Connect the analyser-node to another audio-node
+ * @param  {audioNode} node An audio-node to connect to
+ */
+Oscilloscope.connect(audioNode);
+
+/**
+ * Connect the analyser-node to the audio-context' destination
+ */
+Oscilloscope.toggleAudio();
+````
+
+## License
+Oscilloscope.js is licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ bower_components/oscilloscope.js/oscilloscope.js
@@ -0,0 +1,101 @@
+'use strict';
+/**
+ * Visualise an audio-stream with an oscilloscope
+ * @param  {string} target  The target container, in this container the oscilloscope will be created.
+ * @param  {context} context An audio context containing the audio-stream
+ * @return {Oscilliscope} The Oscilloscope object
+ */
+var Oscilloscope = Oscilloscope || function(target, context) {
+    var _drawWave, _bufferLength, _dataArray;
+
+    this.target = document.querySelector(target);
+
+    // Set the dimensions based on the target container
+    this.width = this.target.offsetWidth;
+    this.height = this.target.offsetHeight;
+
+    // Create the oscilloscopt wave element
+    this.wave = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+    this.wave.setAttribute('class', 'oscilloscope__wave');
+
+    // Create the oscilloscope svg element
+    this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    this.svg.setAttribute('width', this.width);
+    this.svg.setAttribute('height', this.height);
+    this.svg.setAttribute('class', 'oscilloscope__svg');
+    this.svg.appendChild(this.wave);
+
+    // Append the svg element to the target container
+    this.target.appendChild(this.svg);
+
+    // Add the audio context or create a new one
+    this.audioContext = context || new window.AudioContext();
+
+    // Indicates if the oscilloscope is running
+    this.running = false;
+
+    // Is the oscilloscope analyser-node connected to the audio-context' destination
+    this.hasAudio = false;
+
+    // Set-up the analyser-node which we're going to use to get the oscillation wave
+    this.analyserNode = this.audioContext.createAnalyser();
+    this.analyserNode.fftSize = 128;
+    _bufferLength = this.analyserNode.frequencyBinCount;
+    _dataArray = new Uint8Array(_bufferLength);
+
+    /**
+     * Draw the oscillation wave
+     */
+    _drawWave = function() {
+        var path = 'M';
+
+        this.analyserNode.getByteTimeDomainData(_dataArray);
+
+        _dataArray.forEach(function(point, i) {
+            path += (((this.width + (this.width / _bufferLength))/ _bufferLength) * i) + ' ' + ((this.height / 2) * (point / 128.0)) + ', ';
+        }.bind(this));
+
+        this.wave.setAttribute('d', path);
+
+        if (this.running) {
+            window.requestAnimationFrame(_drawWave);
+        }
+    }.bind(this);
+
+    /**
+     * Start the oscilloscope
+     */
+    this.start = function() {
+        this.running = true;
+
+        window.requestAnimationFrame(_drawWave);
+    }.bind(this);
+};
+
+/**
+ * Stop the oscilloscope
+ */
+Oscilloscope.prototype.stop = function() {
+    this.running = false;
+};
+
+/**
+ * Connect the analyser-node to another audio-node
+ * @param  {audioNode} node An audio-node to connect to
+ */
+Oscilloscope.prototype.connect = function(node) {
+    this.analyserNode.connect(node);
+};
+
+/**
+ * Connect the analyser-node to the audio-context' destination
+ */
+Oscilloscope.prototype.toggleAudio = function() {
+    if (!!this.hasAudio) {
+        this.analyserNode.disconnect();
+    } else {
+        this.analyserNode.connect(this.audioContext.destination);
+    }
+
+    this.hasAudio != this.hasAudio;
+};
